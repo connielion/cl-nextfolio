88b09d6aafe7c920387fca1e695269678da95cc1
Connie Lai <clai011@ucr.edu>
2022-05-07 21:35:49 -0700

    feat(app): add next SEO into _app.js and refactored index.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,26 @@
 import "@fortawesome/fontawesome-svg-core/styles.css";
-
+import { DefaultSeo, NextSeo } from "next-seo";
 import "../styling/globals.css";
 import Layout from "../components/layout/layout";
 import { ThemeProvider } from "next-themes";
 import { ParallaxProvider } from "react-scroll-parallax";
-
+import Head from "next/head";
 // layout or persist data
 function MyApp({ Component, pageProps }) {
-  console.log(`Component title: ${Component.title}`);
   return (
     <ThemeProvider>
       <ParallaxProvider>
         <Layout>
+          <Head>
+            <NextSeo
+              title="Connie's Dev Portfolio/Blog"
+              description="Connie Lai react developer portfolio with MDX blog made with Next.js"
+            />
+            {/* UNCOMMENT ONCE SEO CONFIG IS READY */}
+            {/* <DefaultSeo {...SEO} /> */}
+            <link rel="shortcut icon" href="/32xfavicon.png" />
+          </Head>
+
           <Component {...pageProps} />
         </Layout>
       </ParallaxProvider>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
-import Head from "next/head";
 import Hero from "../components/sections/hero";
 import ContactSection from "../components/sections/contact";
 import ProjectsSection from "../components/sections/projects";
 import AboutSection from "../components/sections/about";
 import styled from "styled-components";
-
 const Sections = styled.div`
   display: flex;
   flex-direction: column;
@@ -15,14 +13,6 @@ const Sections = styled.div`
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Connie Lai&rsquo;s Portfolio</title>
-        <meta
-          name="description"
-          content="Connie Lai's developer portfolio made with Next.js"
-        />
-        <link rel="shortcut icon" href="/32xfavicon.png" />
-      </Head>
       {/* Content */}
       <Sections>
         <Hero />
