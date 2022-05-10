3e5878f8ae7976d4670f91d31c85b19b5eef591e
Connie Lai <clai011@ucr.edu>
2022-05-10 14:33:39 -0700

    fix(border-button): replace flex with inline for Btn to show text on mobile

--- components/layout/border-button.js
+++ components/layout/border-button.js
@@ -1,35 +1,39 @@
 import styled from "styled-components";
 import widths from "../../styling/device-sizes";
 
-const Btn = styled.span`
+const Btn = styled.div`
   margin-top: 0.8rem;
   background-image: linear-gradient(135deg, #3c50c1 0%, #22b6b3 100%);
   -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  color: transparent;
   border: 2px solid;
   border-image: linear-gradient(135deg, #3c50c1 0%, #22b6b3 100%);
   border-image-slice: 1;
   font-weight: bold;
   line-height: 1;
-  display: flex;
-  jusify-content: center;
   padding: 0.6rem;
+  min-width: 100px;
+  display: inline;
   transition: all cubic-bezier(0.19, 1, 0.22, 1) 0.6s;
-  &:hover {
-    -webkit-text-fill-color: #a9bed2;
-    text-decoration: none;
-    &::after {
-      width: 100%;
-    }
-  }
   @media (min-width: ${widths.tab}) {
     margin: 1rem;
+  }
+  @media (min-width: ${widths.laptop}) {
+    &:hover {
+      color: #a9bed2;
+      text-decoration: none;
+      &::after {
+        width: 100%;
+      }
+    }
   }
 `;
 
 const ButtonText = styled.p`
   margin: 0 auto;
   font-size: 16px;
+
   @media (min-width: ${widths.laptop}) {
     font-size: 18px;
   }
